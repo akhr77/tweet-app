8f1a3f19e8d07df38c3cc61a4c22b820fca56e4a
Akihiro Nagamine <a.ki.yu.522@gmail.com>
2020-01-30 09:56:48 +0900

    update front:フォルダ構成変更

--- assets/src/views/Home.vue
+++ assets/src/views/Home.vue
@@ -31,7 +31,7 @@
               </div>
               <el-row class="avatar">
                 <el-col :span="12">
-                  <div class="sub-title">@mineko</div>
+                  <div class="sub-title">@みねこ</div>
                   <div class="avater-circle">
                     <div class="block">
                       <el-avatar :size="70" :src="circleUrl"></el-avatar>
@@ -56,8 +56,6 @@ export default {
     return {
       circleUrl:
         "https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"
-      //   squareUrl:
-      //     "https://cube.elemecdn.com/9/c2/f0ee8a3c7c9638a54940382568c9dpng.png"
     };
   }
 };
