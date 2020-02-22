5d3f02b7e77a929dc542f4b37406bea6c91bf5e6
Akihiro Nagamine <a.ki.yu.522@gmail.com>
2020-02-22 11:51:15 +0900

    add 画面の殼を作成

---
+++ assets/src/views/Post.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="post">
+    <el-container>
+      <el-main>
+        <h1>Post ページ</h1>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
