c8c984de69f061752cad36e1c2a2117ef47a0639
Akihiro Nagamine <a.ki.yu.522@gmail.com>
2020-04-05 23:17:36 +0900

    add front 画像投稿時のリサイズ

--- assets/src/views/Post.vue
+++ assets/src/views/Post.vue
@@ -1,22 +1,33 @@
 <template>
   <div id="post">
-    <div class="columns is-desktop">
-      <div class="column" v-if="imageData">
-        <div class="buttuns">
-          <b-button rounded>キャンセル</b-button>
-          <b-button rounded @click="post">投稿</b-button>
-        </div>
-        <img class="image-preview" :src="imageData" />
-      </div>
-      <div class="column" v-else>
-        <div class="image-form">
-          <input type="file" accept="image/*" @change="onFileChange($event)" />
-        </div>
-      </div>
-    </div>
-    <div class="post-form-area">
+    <!-- <div class="post-form-area">
       <i class="fas fa-camera fa-5x post-form"></i>
       <input type="file" accept="image/*" @change="onFileChange($event)" />
+      <img class="preview" :src="imageData" />
+    </div>-->
+
+    <div class="resize-img">
+      <!-- 画像選択 -->
+      <div v-show="!resizedImg" class="resize-img__post">
+        <label for="file" class="resize-img__post__label">
+          投稿画像選択
+          <input
+            id="file"
+            ref="fileInput"
+            type="file"
+            accept=".jpeg, .png"
+            @change="uploadImg"
+          />
+        </label>
+      </div>
+      <!-- プレビュー -->
+      <div v-show="resizedImg" class="resize-img__preview">
+        <canvas ref="canvas" class="resize-img__preview__canvas" />
+        <div class="buttuns">
+          <b-button rounded @click="clearUploadImg">キャンセル</b-button>
+          <b-button rounded @click="post">投稿</b-button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -27,24 +38,14 @@ export default {
     return {
       fileName: "",
       fileType: "",
-      imageData: ""
+      imageData: "",
+      resizedImg: null
     };
   },
+  destroyed() {
+    this.clearUploadImg();
+  },
   methods: {
-    onFileChange(e) {
-      const files = e.target.files;
-      if (files.length > 0) {
-        const file = files[0];
-        const reader = new FileReader();
-        this.fileName = file.name;
-        this.fileType = file.type;
-        reader.onload = e => {
-          this.imageData = e.target.result;
-        };
-        reader.readAsDataURL(file);
-      }
-    },
-
     post() {
       let params = new URLSearchParams();
       params.append("fileName", this.fileName);
@@ -56,25 +57,97 @@ export default {
           console.log(response);
           this.$router.push("list");
         });
+    },
+
+    uploadImg(e) {
+      const file = e.target.files[0];
+      const reader = new FileReader();
+      this.fileName = file.name;
+      this.fileType = file.type;
+      reader.onload = e => {
+        this.generateImgUrl(e.target.result);
+      };
+      reader.readAsDataURL(file);
+    },
+
+    generateImgUrl(file) {
+      const image = new Image();
+      image.crossOrigin = "Anonymous";
+
+      image.onload = () => {
+        const resizedBase64 = this.makeResizeImg(image);
+        this.imageData = resizedBase64;
+        // リサイズ済みのBase64をblobに変換
+        const resizedBlob = this.base64ToBlob(resizedBase64);
+        // urlを生成してプレビュー表示できるようにする
+        const resizedImg = window.URL.createObjectURL(resizedBlob);
+        this.resizedImg = resizedImg;
+      };
+      image.src = file;
+    },
+
+    makeResizeImg(image) {
+      const canvas = this.$refs.canvas;
+      const ctx = canvas.getContext("2d");
+      // 縦横で長い方の最大値を1000
+      const MAX_SIZE = 580;
+
+      if (image.width < MAX_SIZE && image.height < MAX_SIZE) {
+        [canvas.width, canvas.height] = [image.width, image.height];
+        ctx.drawImage(image, 0, 0);
+        return canvas.toDataURL("image/jpeg");
+      }
+
+      let dstWidth;
+      let dstHeight;
+      // 縦横比の計算
+      if (image.width > image.height) {
+        dstWidth = MAX_SIZE;
+        dstHeight = (image.height * MAX_SIZE) / image.width;
+      } else {
+        dstHeight = MAX_SIZE;
+        dstWidth = (image.width * MAX_SIZE) / image.height;
+      }
+      canvas.width = dstWidth;
+      canvas.height = dstHeight;
+      // リサイズ
+      ctx.drawImage(
+        image,
+        0,
+        0,
+        image.width,
+        image.height,
+        0,
+        0,
+        dstWidth,
+        dstHeight
+      );
+
+      return canvas.toDataURL("image/jpeg");
+    },
+
+    clearUploadImg() {
+      this.resizedImg = null;
+      if (this.$refs.fileInput && this.$refs.fileInput.value !== undefined) {
+        this.$refs.fileInput.value = "";
+      }
+    },
+
+    base64ToBlob(base64) {
+      const bin = atob(base64.replace(/^.*,/, ""));
+      const buffer = new Uint8Array(bin.length);
+      for (let i = 0; i < bin.length; i++) {
+        buffer[i] = bin.charCodeAt(i);
+      }
+      return new Blob([buffer.buffer], {
+        type: "image/png"
+      });
     }
   }
 };
 </script>
 
 <style scoped>
-.image-form {
-  max-width: 500px;
-  height: 300px;
-  background-color: red;
-  border-radius: 1rem;
-  margin-left: auto;
-  margin-right: auto;
-}
-
-.image-preview {
-  max-width: 500px;
-}
-
 .post-form-area {
   max-width: 550px;
   height: 300px;
